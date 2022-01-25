ec35d710303baebfd1577aec18faf09034aed51b
Clement Le Boulanger <hello@clement-leboulanger.com>
2022-01-25 08:49:11 +0100

    Fix Movie Element

--- src/components/movie-list/MovieElement/MovieElement.js
+++ src/components/movie-list/MovieElement/MovieElement.js
@@ -3,13 +3,13 @@ import Style from './MovieElement.module.scss';
 
 export default class MovieElement extends Component {
 
-  mouseEnter = () => {
+  mouseClick = () => {
     this.props.updateSelectedMovie(this.props.movie.title)
   }
 
   render(){
     return (
-      <div onMouseEnter={this.mouseEnter} className={ "d-flex bg-light shadow flex-row " + Style.container }>
+      <div onClick={this.mouseClick} className={ "d-flex bg-light shadow flex-row " + Style.container }>
         <img width="185" src={ this.props.movie.img } alt=""/>
         <div className="flex-fill d-flex flex-column px-3 pt-3">
           <h5>{ this.props.movie.title }</h5>
